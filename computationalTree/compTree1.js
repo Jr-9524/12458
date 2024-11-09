dadd296ea68a2da451783fd271ed770da2965ea6
Jr-9524 <jhonruzzelolan2@gmail.com>
2024-11-09 23:32:18 +0800

    Add files via upload

---
+++ computationalTree/compTree1.js
@@ -0,0 +1,32 @@
+// nested objects
+// Strings that starts with '0'
+const transitions = {
+    'q0': { '0': 'q1', '1': 'p' }, 
+    'q1': { '0': 'q1', '1': 'q1' }
+};
+
+function test() {
+    const input = document.getElementById('inputString').value.trim(); // dito mapunta yung value ng tinype mo. Ignore whitespaces
+
+    let currentState = 'q0';
+    let accepted = false;
+    const transitionLog = []; 
+    document.getElementById('initial').innerText = `Start State ${currentState}`;
+    for (let i = 0; i < input.length; i++) {
+        const element = input[i];
+
+        if (transitions[currentState] && transitions[currentState][element]) {
+            const nextState = transitions[currentState][element];
+            transitionLog.push(`${element} → '${currentState}' → '${nextState}'`); // yung .push eii mag lagay lang ng maglagay ng element sa array
+            currentState = nextState;
+        } else {
+            currentState = 'p';
+            break;
+        }
+    }
+
+    accepted = currentState === 'q1' && input.startsWith('0');
+    document.getElementById('result').innerText = accepted ?
+        `The string "${input}" is accepted.\nTransitions:\n${transitionLog.join('\n')}` : // .join, pagsamasamahin yung mga element ng array
+        `The string "${input}" is not accepted.\nTransitions:\n${transitionLog.join('\n')}`;
+}
